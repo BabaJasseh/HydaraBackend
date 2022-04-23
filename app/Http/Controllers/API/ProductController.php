1d5f6880cb7c2e2d5c4618c5bfac34b6d7d8ef46
MagnificentTerminator <majaboujallow@gmail.com>
2022-04-23 14:49:44 +0000

    product-limit-updated

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -50,10 +50,11 @@ class ProductController extends Controller
     {
         // $product = Product::with('category', 'brand', 'stock')->get();
         // return $request;
+        $limit = $request->limit;
         if ($request->sort == "-id") {
-            $product = Product::with('category', 'brand', 'stock')->orderBy('id', 'desc')->paginate(20);
+            $product = Product::with('category', 'brand', 'stock')->orderBy('id', 'desc')->paginate($limit);
         } else {
-            $product = Product::with('category', 'brand', 'stock')->paginate(20);
+            $product = Product::with('category', 'brand', 'stock')->paginate($limit);
         }
 
         if ($request->name) {
@@ -63,7 +64,7 @@ class ProductController extends Controller
                     'category',
                     'brand',
                     'stock'
-                )->orderBy('id', $order)->paginate(20);
+                )->orderBy('id', $order)->paginate($limit);
         }
         $response = [
             'pagination' => [
