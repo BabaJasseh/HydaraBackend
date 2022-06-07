e42c86702b0d72e08fdb547b8f152a1cb31efcb8
MagnificentTerminator <majaboujallow@gmail.com>
2022-06-07 12:54:44 +0000

    before-deployment-final

---
+++ app/Models/BorrowerTransaction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Borrower;
+
+class BorrowerTransaction extends Model
+{
+    use HasFactory;
+
+
+
+    public function borrower()
+    {
+        return $this->belongsTo(Borrower::class);
+    }
+}
